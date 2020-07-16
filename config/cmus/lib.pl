55cc26a2a029532b9d57f21c23bcfb38c3ab0d2c
onelonlyprogrammer <mylrumph@mail.com>
2020-07-15 21:26:58 -0400

    Update Files

---
+++ config/cmus/lib.pl
@@ -0,0 +1,160 @@
+/home/myles/2020-06-21 20-07-00.mp4
+/home/myles/2020-06-21 20-08-12.mp4
+/home/myles/2020-06-21 20-19-46.mp4
+/home/myles/Desktop/Godot-Sonic-Physics-master/Source Code/Audio/sfx/brake.wav
+/home/myles/Desktop/Godot-Sonic-Physics-master/Source Code/Audio/sfx/charge.wav
+/home/myles/Desktop/Godot-Sonic-Physics-master/Source Code/Audio/sfx/instaShield.wav
+/home/myles/Desktop/Godot-Sonic-Physics-master/Source Code/Audio/sfx/jump.wav
+/home/myles/Desktop/Godot-Sonic-Physics-master/Source Code/Audio/sfx/peelOutCharge.wav
+/home/myles/Desktop/Godot-Sonic-Physics-master/Source Code/Audio/sfx/peelOutRelease.wav
+/home/myles/Desktop/Godot-Sonic-Physics-master/Source Code/Audio/sfx/spinDashCharge.wav
+/home/myles/Desktop/Godot-Sonic-Physics-master/Source Code/Audio/sfx/spinDashRelease.wav
+/home/myles/Desktop/Godot-Sonic-Physics-master/Source Code/Audio/sfx/spin.wav
+/home/myles/Desktop/Sonic Test/Source Code/Audio/sfx/brake.wav
+/home/myles/Desktop/Sonic Test/Source Code/Audio/sfx/charge.wav
+/home/myles/Desktop/Sonic Test/Source Code/Audio/sfx/instaShield.wav
+/home/myles/Desktop/Sonic Test/Source Code/Audio/sfx/jump.wav
+/home/myles/Desktop/Sonic Test/Source Code/Audio/sfx/peelOutCharge.wav
+/home/myles/Desktop/Sonic Test/Source Code/Audio/sfx/peelOutRelease.wav
+/home/myles/Desktop/Sonic Test/Source Code/Audio/sfx/spinDashCharge.wav
+/home/myles/Desktop/Sonic Test/Source Code/Audio/sfx/spinDashRelease.wav
+/home/myles/Desktop/Sonic Test/Source Code/Audio/sfx/spin.wav
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 02.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 03.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 06.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 07.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 08.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 09.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 14.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 18.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 20.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 26.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 27.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 28.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 31.mp3
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 02.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 03.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 04.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 05.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 06.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 07.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 08.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 09.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 10.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 11.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 12.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 13.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 14.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 15.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 16.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 17.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 18.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 19.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 20.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 21.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 22.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 23.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 24.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 25.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 26.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 27.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 28.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 29.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 30.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 31.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 32.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 33.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 34.wav
+/home/myles/Documents/Sonic_CD++(WAVE)/Sonic the Hedgehog CD (US) - Track 35.wav
+/home/myles/Music/Music/I Found A Diamond.mp3
+/home/myles/Music/Recorders/2019_11_21_16_40_32.mp3
+/home/myles/Music/Recorders/2019_11_21_16_40_42.mp3
+/home/myles/Music/Recorders/2019_11_21_16_41_23.mp3
+/home/myles/Music/Recorders/2019_11_21_16_42_18.mp3
+/home/myles/Music/Recorders/2019_11_23_14_32_15.mp3
+/home/myles/Music/Recorders/2020_02_07_16_00_52.mp3
+/home/myles/Music/Recorders/2020_02_07_16_03_39.mp3
+/home/myles/Music/Recorders/2020_02_07_16_36_25.mp3
+/home/myles/Music/Recorders/2020_02_13_17_13_52.mp3
+/home/myles/Music/Recorders/2020_02_13_17_36_08.mp3
+/home/myles/Music/Security Footage Shows George Floyd Clearly Drop A Small Baggie w White Substance During Arrest.mp4
+/home/myles/Music/Sonic the Hedgehog CD (US) - Track 07.mp3
+/home/myles/Music/Sonic the Hedgehog CD (US) - Track 09.mp3
+/home/myles/Videos/Minneapolis Cop Kills George Floyd In Front Of Onlookers.mp4
+/home/myles/Videos/Minneapolis Cop Kills George Floyd In Front Of Onlookers(Rightside Up).mp4
+/home/myles/Videos/simplescreenrecorder.mkv
+/home/myles/Videos/Sonic Heroes 1-2020-06-22_09.06.36.mkv
+/home/myles/Videos/Sonic Heroes 1-2020-06-22_09.21.04.mkv
+/home/myles/Music/14._zoras_domain_remix.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 23.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/Guggenz - Chillhop Essentials - Summer 2016 - 01 Business As Usual.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/Joakim Karud - Chillhop Essentials - Summer 2016 - 02 Canals.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/Birocratic - Chillhop Essentials - Summer 2016 - 03 Tony's Belated Breakfast.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/Brock Berrigan - Chillhop Essentials - Summer 2016 - 04 Split Decision.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/Floppy Circus - Chillhop Essentials - Summer 2016 - 05 Better Days To Come.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/Handbook - Chillhop Essentials - Summer 2016 - 06 Melon.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/Phoniks - Chillhop Essentials - Summer 2016 - 07 Touchdown.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/Gorila - Chillhop Essentials - Summer 2016 - 08 Heatinupyabody.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/Bonus Points - Chillhop Essentials - Summer 2016 - 09 Hammock Days.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/Mecca-83 - Chillhop Essentials - Summer 2016 - 10 Foundation.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/GlobulDub - Chillhop Essentials - Summer 2016 - 11 Waves.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/deeB - Chillhop Essentials - Summer 2016 - 12 Theme From Endless Sunset.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/Imagined Herbal Flows - Chillhop Essentials - Summer 2016 - 13 Beyond The Sun.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/Aso - Chillhop Essentials - Summer 2016 - 14 Ultra Violet.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/White Lights - Chillhop Essentials - Summer 2016 - 15 VR Helmet.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/Philanthrope - Chillhop Essentials - Summer 2016 - 16 Isolation.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/Ol' Burger Beats - Chillhop Essentials - Summer 2016 - 17 The Gentle Instrumental.mp3
+/home/myles/Music/Music/Chillhop Records - Chillhop Essentials - Summer 2016/Jeff Kaale - Chillhop Essentials - Summer 2016 - 18 Footsteps.mp3
+/home/myles/Desktop/Sonic Test/Source Code/Audio/musics/Dan Gravelle - Green Hill Zone [Sonic The Hedgehog].MP3
+/home/myles/Desktop/Sonic Test/Source Code/Audio/musics/Dan Gravelle - Green Hill Zone [Sonic The Hedgehog].ogg
+/home/myles/kazam/pkg/kazam/usr/share/kazam/sounds/Canon 7D.ogg
+/home/myles/kazam/src/kazam-1.4.5/data/sounds/Canon 7D.ogg
+/home/myles/kazam/pkg/kazam/usr/share/kazam/sounds/Nikon D80.ogg
+/home/myles/kazam/src/kazam-1.4.5/data/sounds/Nikon D80.ogg
+/home/myles/Music/Music/The Music.mp3
+/home/myles/Music/Music/Fumaza.mp3
+/home/myles/Music/Music/13 - Blessing Endear.mp3
+/home/myles/Music/Music/Music By M.C/Quest Awakeing.mp3
+/home/myles/Music/Music/Music By M.C/Realization .mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 12.mp3
+/home/myles/Music/Music/Perséphone - Retro Funky (SUNDANCE Remix).mp3
+/home/myles/Music/Music/PureNRG/A PureNRG Christmas/01 Joy to the World.wav
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 34.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 16.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 04.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 11.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 19.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 29.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 22.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 17.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 15.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 25.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 21.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 24.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 10.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 05.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 13.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 32.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 30.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 33.mp3
+/home/myles/Documents/Sonic_CD++/Sonic the Hedgehog CD (US) - Track 35.mp3
+/home/myles/Music/Music/Tee Lopes/Tee_Lopes_-_Green_Hill_Zone_12[MP3orMP4.com].mp3
+/home/myles/Music/Music/Tee Lopes/Tee_Lopes_-_Labyrinth_Zone_12[MP3orMP4.com].mp3
+/home/myles/Music/Music/Tee Lopes/Tee_Lopes_-_Marble_Zone_12[MP3orMP4.com].mp3
+/home/myles/Music/Music/Tee Lopes/Tee_Lopes_-_Scrap_Brain_12[MP3orMP4.com].mp3
+/home/myles/Music/Music/Tee Lopes/Tee_Lopes_-_Spring_yard_zone_Re-imagined[MP3orMP4.com].mp3
+/home/myles/Music/Music/Tee Lopes/Tee_Lopes_-_Starlight_zone_Re-Imagined[MP3orMP4.com].mp3
+/home/myles/Music/Music/Babylon of the Orient.mp3
+/home/myles/Desktop/Godot-Sonic-Physics-master/Source Code/Audio/musics/sky_sanctuary_zone.ogg
+/home/myles/Desktop/Sonic Test/Source Code/Audio/musics/sky_sanctuary_zone.ogg
+/home/myles/Music/Music/Trebles and Blues - The Blue Note (2011)/01 The Tempo.mp3
+/home/myles/Music/Music/Trebles and Blues - The Blue Note (2011)/02 The First Time.mp3
+/home/myles/Music/Music/Trebles and Blues - The Blue Note (2011)/03 808s and Quintets.mp3
+/home/myles/Music/Music/Trebles and Blues - The Blue Note (2011)/04 Peace in Everything.mp3
+/home/myles/Music/Music/Trebles and Blues - The Blue Note (2011)/05 Slum Love Japan (Interlude).mp3
+/home/myles/Music/Music/Trebles and Blues - The Blue Note (2011)/06 An Evening in Osaka.mp3
+/home/myles/Music/Music/Trebles and Blues - The Blue Note (2011)/07 The Soulmate that Seldom Speaks.mp3
+/home/myles/Music/Music/Trebles and Blues - The Blue Note (2011)/08 131 West 3rd Street.mp3
+/home/myles/Music/Music/Trebles and Blues - The Blue Note (2011)/09 Win Against Them All.mp3
+/home/myles/Music/Music/Trebles and Blues - The Blue Note (2011)/10 Pluto Jazz.mp3
+/home/myles/Music/Music/Trebles and Blues - The Blue Note (2011)/11 Every Mind Has Changed (Outro).mp3
---
+++ config/libreoffice/4/user/uno_packages/cache/uno_packages/lu192308uv59fs.tmp_/0ec1c2e8_access2base.oxt/description/readme.txt
@@ -0,0 +1,11 @@
+An ("MSAccess-like") API for LibreOffice/OpenOffice Base
+
+Current version is PRE-INSTALLED IN LIBREOFFICE 6.1 AND LATER.
+INSTALL ONLY UNDER OPENOFFICE (all versions) OR LIBREOFFICE (versions <= 6.0) !!
+READ http://www.access2base.com/access2base.html#Install FOR MORE DETAILS.
+
+Access2Base is a LibreOffice/OpenOffice extension for (business or personal) application developers and advanced users.
+It provides a LibO/AOO Basic library of macros mainly inspired by VBA for MSAccess and its object model.
+The library is much more intuitive and easy to learn than the standard UNO API.
+
+Documentation is available in the Basic IDE (Help menu) or on http://www.access2base.com.
